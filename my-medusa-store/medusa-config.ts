b9fe84eaa05299bc67aaf095c742ecc46b4883d4
Doan0904 <dangnhatdoan@gmail.com>
2026-04-06 17:28:03 +0000

    Thêm minIO

--- my-medusa-store/medusa-config.ts
+++ my-medusa-store/medusa-config.ts
@@ -25,5 +25,31 @@ admin: {
     disable: false, 
     // Trỏ về đúng domain của bạn [cite: 32]
     backendUrl: process.env.MEDUSA_BACKEND_URL || "http://localhost:9000"
-  }
+  },
+// THÊM MODULE CHO MINIO
+modules: [
+    {
+      resolve: "@medusajs/medusa/file",
+      options: {
+        providers: [
+          {
+            resolve: "@medusajs/file-s3",
+            id: "s3",
+            options: {
+              // 👉 SỬA LẠI ĐÚNG LINK API CỦA BẠN:
+              file_url: "https://zang-minio-api.app.lilichilly.com/medusa-media",
+              endpoint: "https://zang-minio-api.app.lilichilly.com",
+              
+              access_key_id: process.env.S3_ACCESS_KEY_ID,
+              secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
+              bucket: "medusa-media", // Đảm bảo bạn đã tạo bucket tên này trong MinIO
+              region: "us-east-1",
+              force_path_style: true,
+            },
+          },
+        ],
+      },
+    },
+  ]
+
 })
