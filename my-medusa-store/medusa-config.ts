cd7a3204ce7f2129be09d32b1e5858329f1fd12f
Doan0904 <dangnhatdoan@gmail.com>
2026-04-09 14:49:21 +0000

    Sửa lại file medusa-config

--- my-medusa-store/medusa-config.ts
+++ my-medusa-store/medusa-config.ts
@@ -5,11 +5,11 @@ loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 const safeBackendUrl = process.env.MEDUSA_BACKEND_URL 
   ? process.env.MEDUSA_BACKEND_URL.replace(/\/$/, '') 
   : "https://zang-finance-app.app.lilichilly.com";
-export default defineConfig({
+
+module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
     redisUrl: process.env.REDIS_URL,
-    // SỬA ĐOẠN NÀY: Ép buộc tắt SSL để khớp với cấu hình Database trên CapRover
     databaseDriverOptions: {
       connection: {
         ssl: false
@@ -21,20 +21,14 @@ export default defineConfig({
       authCors: process.env.AUTH_CORS || "http://localhost:7001",
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-
-	// Mở cửa thư mục uploads
     }
   },
-admin: {
-    // Ép backend phục vụ admin dashboard 
+  admin: {
     disable: false, 
-    // Trỏ về đúng domain của bạn [cite: 32]
     backendUrl: process.env.MEDUSA_BACKEND_URL || "http://localhost:9000"
   },
-// THÊM MODULE CHO BDD
-modules: [
- 
-{
+  modules: [
+    {
       resolve: "@medusajs/medusa/file",
       options: {
         providers: [
@@ -42,27 +36,20 @@ modules: [
             resolve: "@medusajs/file-local",
             id: "local",
             options: {
-              // Thư mục chứa ảnh (Medusa sẽ tự tạo thư mục này trong source code)
               upload_dir: "static/uploads",
-              
-              // Để Medusa biết đường tạo link ảnh public trả về cho frontend
               backend_url: `${safeBackendUrl}/static/uploads`,
             },
           },
         ],
       },
     },
-
-	{
+    {
       resolve: "@medusajs/payment-stripe",
       options: {
         api_key: process.env.STRIPE_API_KEY,
         webhook_secret: process.env.STRIPE_WEBHOOK_SECRET,
-        // Bật tính năng tự động nhận diện phương thức thanh toán để hiện VNPay
         automatic_payment_methods: true, 
       },
     },
-
   ]
 })
-
