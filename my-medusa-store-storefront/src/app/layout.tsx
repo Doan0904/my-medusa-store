864e6dc4360280376b59849c8284a17e8904df99
Doan0904 <dangnhatdoan@gmail.com>
2026-04-09 17:22:22 +0000

    Tắt extention chèn code

--- my-medusa-store-storefront/src/app/layout.tsx
+++ my-medusa-store-storefront/src/app/layout.tsx
@@ -8,8 +8,9 @@ export const metadata: Metadata = {
 
 export default function RootLayout(props: { children: React.ReactNode }) {
   return (
-    <html lang="en" data-mode="light">
-      <body>
+    {/* Thêm suppressHydrationWarning vào html và body */}
+    <html lang="en" data-mode="light" suppressHydrationWarning>
+      <body suppressHydrationWarning>
         <main className="relative">{props.children}</main>
       </body>
     </html>
